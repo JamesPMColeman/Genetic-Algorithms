702396cd8d17d6be312213c62424326d13a68e72
James Coleman <jcolem22@msudenver.edu>
2020-12-17 16:55:46 -0700

    New attempt at One-max using mix
    Using elixir's mix (builder ?) I have set up a frame more in-depth
    framework for solving the one max problem. Currently I have built
    a run, initialize and evolve function in the genetic.ex file.

---
+++ genetic/genetic/lib/genetic.ex
@@ -0,0 +1,26 @@
+def initialize(genotype) do
+  for _ <- 1..100, do: genotype.()
+end
+
+def run(...) do
+
+  population = initialize()
+  population
+  |> evolve()
+
+end
+
+def evolve(population, max_fitness) do
+  population = evaluate(population, ..., opts)
+  best = hd(population)
+  IO.write("\rCurrent Best: ...")
+  if ... == max_fitness do
+	best
+  else
+	population
+	|> select()
+	|> crossover()
+	|> mutation()
+	|> evolve()
+  end
+end
