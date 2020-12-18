00d963b93b3c2363bea33ddeea77137ba7689f2c
James Coleman <jcolem22@msudenver.edu>
2020-12-17 17:26:23 -0700

    Added functions crossover/1 and mutation/1
    crossover/1 takes a pair of chromosomes and exchanges genes after
    a random point to produce two new child chromosomes. mutation/1
    chooses chromosomes at random (5%) to mutate by scrambling the
    genes.

--- genetic/genetic/lib/genetic.ex
+++ genetic/genetic/lib/genetic.ex
@@ -13,6 +13,33 @@ def select(population) do
   |> Enum.map(&List.to_tuple(&1))
 end
 
+def crossover(population) do
+  population
+  |> Enum.reduce([],
+	  fn {p1, p2}, acc ->
+	    cx_point = :rand.uniform(length(p1))
+		{{h1, t1}, {h2, t2}} =
+		  {Enum.split(p1, cx_point),
+		  Enum.split(p2, cx_point)}
+		{c1, c2} = {h1 ++ t2, h2 ++ t1}
+		[c1, c2 | acc]
+	  end
+	)
+end
+
+def mutation(population) do
+  population
+  |> Enum.map(
+	  fn chromosome ->
+		if :rand.unifor() < 0.05 do
+		  Enum.shuffle(chromosome)
+		else
+		  chromosome
+		end
+	  end
+	)
+end
+
 def run(...) do
 
   population = initialize()
